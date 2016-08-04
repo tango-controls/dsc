970191a0375716c030ced0bb3a6f8a37dc457638
Piotr Goryl <piotr.goryl@3-controls.com>
2016-08-04 10:30:08 +0200

    Frame for views id DSC prepared

--- views.py
+++ views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here
+
+
+def index(request):
+    '''
+    Renders index page
+    :param request: HTTP request provided by the Django
+    :return: return to be
+    '''
+    return HttpResponse("Hello, world. You're at the DS Catalogue index.")
+
+def ds_detail(request):
+    return HttpResponse("Hello, world. Soon, you will see DS detail here.")
