15a0e82630658933c5dbb1c37eea83cd209d14a0
Piotr Goryl <piotr.goryl@3-controls.com>
2016-09-23 17:04:17 +0200

    Django bug #24765 over come implemented - it is not yet nice solution but works.

--- cms_plugins.py
+++ cms_plugins.py
@@ -13,6 +13,8 @@ from dsc.tables import DeviceServerTable
 
 from django_tables2 import RequestConfig
 
+from django.template import RequestContext
+
 class DeviceServerPlugin(CMSPluginBase): #LastPublishedObjectPluginBase
     # TODO device server plugin filters etc
     model = DeviceServerPluginModel
@@ -25,13 +27,36 @@ class DeviceServerPlugin(CMSPluginBase): #LastPublishedObjectPluginBase
 
     def render(self, context, instance, placeholder):
         context = super(DeviceServerPlugin, self).render(context, instance, placeholder)
-        #context['table_class'] = DeviceServerTable
+        context['table_class'] = DeviceServerTable
         table = DeviceServerTable(DeviceServer.objects.all())
         #RequestConfig(request).configure(table)
-        context['table'] = table
-        #context['table_pagination'] = {
-        #    'per_page': 20
-        #}
+        context['table']= table
+        context['table_pagination'] = {
+            'per_page': 20
+        }
+
+
+        # clean context from nested context to get around of bug in context.flatten (django tracker #24765)
+
+        context_inside = True
+        # print type(context.dicts)
+
+        while context_inside:
+            # print '*******************************'
+            new_context_dicts = []
+            context_inside = False
+            for d in context.dicts:
+                # print type(d)
+                # print '-----------------------------------'
+                # print d
+                if isinstance(d, RequestContext):
+                    new_context_dicts.extend(d.dicts)
+                    context_inside = True
+                else:
+                    new_context_dicts.append(d)
+            context.dicts = new_context_dicts
+
+
         return context
 
 plugin_pool.register_plugin(DeviceServerPlugin)
