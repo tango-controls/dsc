d3e749d35f67126a31b250998d0d7ff8829b6b63
Piotr Goryl <piotr.goryl@3-controls.com>
2016-09-23 13:34:10 +0200

    Debuging django-tables2 usage - the plugin is now broken

--- cms_plugins.py
+++ cms_plugins.py
@@ -16,7 +16,7 @@ from django_tables2 import RequestConfig
 class DeviceServerPlugin(CMSPluginBase): #LastPublishedObjectPluginBase
     # TODO device server plugin filters etc
     model = DeviceServerPluginModel
-    name = _('Device Servers Catalogue')
+    name = 'Device Servers Catalogue'
     render_template = "dsc/deviceserver_list.html"
 
     def get_instance_queryset(self, instance, request):
@@ -25,10 +25,13 @@ class DeviceServerPlugin(CMSPluginBase): #LastPublishedObjectPluginBase
 
     def render(self, context, instance, placeholder):
         context = super(DeviceServerPlugin, self).render(context, instance, placeholder)
-        context['table_class'] = DeviceServerTable
+        #context['table_class'] = DeviceServerTable
         table = DeviceServerTable(DeviceServer.objects.all())
         #RequestConfig(request).configure(table)
         context['table'] = table
+        #context['table_pagination'] = {
+        #    'per_page': 20
+        #}
         return context
 
 plugin_pool.register_plugin(DeviceServerPlugin)
--- tables.py
+++ tables.py
@@ -3,16 +3,17 @@ from models import DeviceServer, DeviceServerActivity
 from django_tables2.utils import A
 
 class DeviceServerTable(tables.Table):
-    repository = tables.Column(accessor='repository.path_in_repository')
+    #repository = tables.Column(accessor='repository.path_in_repository')
     #TODO add activitytype = download and here should be the sum
-    downloads = tables.Column(accessor='activities.activity_type', verbose_name= 'Downloads')
+    #downloads = tables.Column(accessor='activities.activity_type', verbose_name= 'Downloads')
     #TODO NO category in the model - must be add
-    name = tables.LinkColumn() #'deviceserver_detail', args=[A('pk')], empty_values=()
-    selection = tables.CheckBoxColumn(accessor="pk", attrs = { "th__input":
-                                        {"onclick": "toggle(this)"}},
-                                        orderable=False)
+    #name = tables.LinkColumn() #'deviceserver_detail', args=[A('pk')], empty_values=()
+    #selection = tables.CheckBoxColumn(accessor="pk", attrs = { "th__input":
+    #                                    {"onclick": "toggle(this)"}},
+    #                                     orderable=False)
+
     class Meta:
         model = DeviceServer
-        fields = ('license','name')
-        sequence = ('selection', 'name', 'repository', 'license', 'downloads',)
+        fields = ('name',)
+        # sequence = ('selection', 'name',) #'repository', 'license', 'downloads',)
 
